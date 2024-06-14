ae5f05ba7cad4d85068153af1826fb09706844d3
Ludmila Berto <ludmilaberto5@gmail.com>
2024-06-14 04:03:55 -0300

    decalre 3 las variables para los numero y 3 variables para los resultados  en el archivo ejercicio4 de js

---
+++ js/ejercicio4.js
@@ -0,0 +1,16 @@
+
+let numeroA=38
+let numeroB=52
+let numeroC=87
+let resultadoSuma=0
+let resultadoResta=0
+let resultadoMultiplica=0
+
+resultadoSuma=numeroA+numeroB+numeroC
+
+resultadoResta=numeroA-numeroB-numeroC
+
+resultadoMultiplica=numeroA*numeroB*numeroC
+
+console.log(`El resultado de la suma es ${resultadoSuma}, de la resta es ${resultadoResta} y el de la multiplicación es ${resultadoMultiplica}`)
+
