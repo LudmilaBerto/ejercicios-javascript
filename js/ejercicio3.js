c64724dda1cfdd4eb53bfe16c767ad0a59d5f377
Ludmila Berto <ludmilaberto5@gmail.com>
2024-06-14 03:05:46 -0300

    Realice el ejercicio 3 ubicado en el archivo ejercicio3.js

---
+++ js/ejercicio3.js
@@ -0,0 +1,6 @@
+
+let numeroEntero = 3
+
+/*Numero primo: son mayores a 1 y no son divisibles por los numeros entre 2 y su raiz cuadrada*/
+
+numeroEntero<=1?console.log('No es un numero primo'):console.log('Es un numero primo')
