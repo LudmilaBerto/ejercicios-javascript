b2f3a83eca7ade41557cdbe6cb4182b979642891
Ludmila Berto <ludmilaberto5@gmail.com>
2024-06-15 03:57:28 -0300

    Corregí errores en el ejercicio 2

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,22 +1,24 @@
 
-let peso=70
+let peso = 98
 
 let altura=1.70
 
-let imc = 70 / (altura*altura)
+let imc = 0
+
+imc = peso / (altura*altura)
 
 console.log('El IMC de una persona es de:', imc)
 
 if(imc<18.5){
     console.log('El IMC es de bajo peso')
 }else{
-    if(18.5 < imc < 24.9){
+    if( imc>18.5 && imc < 24.9){
         console.log('El IMC es normal')
     }else{
-        if(25<imc< 29.9){
+        if(imc>25 && imc< 29.9){
             console.log('El IMC es de sobrepeso')
         }else{
-            if(img>=30){
+            if(imc>=30){
                 console.log('El IMC es de sobrepeso')
             }
         }
